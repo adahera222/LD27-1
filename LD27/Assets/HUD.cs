0bbc2d14cedd1472915b4e3eeda7b659bed4f403
pettipas <paul.pettipas@gmail.com>
2013-08-23 22:54:09 -0300

    the mandatory countdown

---
+++ LD27/Assets/HUD.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+    public float countDown = 10;
+
+    public void Update() { 
+        countDown -= Time.deltaTime;
+        float clamped = Mathf.Clamp(countDown, 0, 10);
+        string val = countDown > -1 ? clamped.ToString("0") + "" : "GAME OVER";
+        guiText.text = val;
+    }
+}
