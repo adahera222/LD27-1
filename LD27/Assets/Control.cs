06e79d9a8ba1eb1df37471b55f1ebb450a06d094
pettipas <paul.pettipas@gmail.com>
2013-08-24 00:12:19 -0300

    movement scheme

---
+++ LD27/Assets/Control.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * dr critical
+ */
+public class Control : MonoBehaviour {
+    
+    public Transform body;
+    public float x;
+    public float z;
+    public float facing;
+
+    public float timeX = 0;
+    public float timeZ = 0;
+    public float timeY = 0;
+
+    public float speed = 10;
+    
+    public void Update() {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+      
+
+        body.localEulerAngles = new Vector3(0, facing, 0);
+        if(Mathf.Abs(x) > 0 || Mathf.Abs(z)>0) UpdateFacing();
+
+        if (Mathf.Abs(z) > 0) {
+            timeZ += Time.deltaTime;
+        }
+        else {
+            timeZ = 0;
+        }
+        if (Mathf.Abs(x) > 0) {
+            timeX += Time.deltaTime;
+        }
+        else {
+            timeX = 0;
+        }
+
+        TimedInputMove();
+    }
+
+    public void UpdateFacing() {
+        if (x == 0 && Mathf.Sign(z) == 1) {//north
+            facing =  0;
+        } else if (x == 0 && Mathf.Sign(z) == -1) {//south
+            facing =  -180;
+        }else if (Mathf.Sign(x) == 1 && z == 0) {//east
+            facing  = -270;
+        }else if (Mathf.Sign(x) == -1 && z == 0) {//west
+            facing =  -90;
+        }
+    }
+
+    public void TimedInputMove() {
+
+        if (timeX == timeZ && Mathf.Abs(timeX) > 0) {
+            if (Random.Range(0, 100) > 50) {
+                timeX += Time.deltaTime;
+            }
+            else {
+                timeZ += Time.deltaTime;
+            }
+        }
+
+        if (timeX > timeZ) {
+            transform.Translate(new Vector3(Mathf.Sign(x) * speed * Time.deltaTime, 0, 0));
+        } else if (timeZ > timeX) {
+            transform.Translate(new Vector3(0, 0, Mathf.Sign(z) * speed * Time.deltaTime));
+        }
+    }
+   
+}
---
+++ LD27/Assets/MeshTimer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshTimer: MonoBehaviour {
+    public TextMesh textMesh;
+    public float countDown = 10;
+
+    public void Update() { 
+        countDown -= Time.deltaTime;
+        float clamped = Mathf.Clamp(countDown, 0, 10);
+        string val = countDown > -1 ? clamped.ToString("0") + "" : "GAME OVER";
+        textMesh.text = val;
+    }
+}
