f5725f59f61b443bba7ba3ece1aaa936a40c6d5a
pettipas <paul.pettipas@gmail.com>
2013-08-25 16:48:34 -0300

    teleporters set up

---
+++ LD27/Assets/gamelogic/ExplosivePotion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosivePotion : MonoBehaviour {
+
+    public void Start() {
+        Collider [] things = Physics.OverlapSphere(transform.position, 5);
+        foreach (Collider c in things) { 
+        
+        }
+    }
+
+   
+}
